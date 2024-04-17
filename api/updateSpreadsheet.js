0fb749dec2ebb175e57c035cb8467ebd62af4d27
Meaghan Campbell <emeaghancampbell@gmail.com>
2024-04-17 10:54:30 -0400

    update server

--- api/updateSpreadsheet.js
+++ api/updateSpreadsheet.js
@@ -8,15 +8,21 @@ const auth = new google.auth.GoogleAuth({
 })
 
 module.exports = async (req, res) => {
-    const sheets = google.sheets({version:'v4', auth});
-    const spreadsheetId = process.env.SPREADSHEET_ID;
-    const valueInputOption = 'USER_ENTERED';
-    const resource = {values: req.body.values};
 
     // Set CORS headers - update * to your website when ready for prod
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    const sheets = google.sheets({version:'v4', auth});
+    const spreadsheetId = process.env.SPREADSHEET_ID;
+    const valueInputOption = 'USER_ENTERED';
+    const resource = {values: req.body.values};
 
     try {
         // Get the data from the sheet
